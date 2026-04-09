a1fdcf372932c23ccfc99d71c85de282edb59b9d
kaylee-weso <kaw0117@auburn.edu>
2026-04-09 14:02:26 -0500

    "login ui pt 2"

--- app/(routes)/login/page.tsx
+++ app/(routes)/login/page.tsx
@@ -24,7 +24,7 @@ export default function LoginPage() {
   };
 
 return (
-    <div className="grid min-h-svh lg:grid-cols-2">
+    <div className="grid min-h-screen lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center items-center gap-2 md:justify-start">
           <img
