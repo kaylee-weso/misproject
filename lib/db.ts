6237be5bb526065a0931825c115b0a52f7f426be
kaylee-weso <kaw0117@auburn.edu>
2026-04-08 22:14:07 -0500

    "Ensure datbase connection"

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
