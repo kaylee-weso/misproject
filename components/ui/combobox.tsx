84950df9b438c23841cae795e49bd5324b6a9cd8
kaylee-weso <kaw0117@auburn.edu>
2026-04-08 23:04:52 -0500

    "Combobox edit"

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -92,12 +92,11 @@ function ComboboxContent({
   sideOffset = 6,
   align = "start",
   alignOffset = 0,
-  anchor,
   ...props
 }: ComboboxPrimitive.Popup.Props &
   Pick<
     ComboboxPrimitive.Positioner.Props,
-    "side" | "align" | "sideOffset" | "alignOffset" | "anchor"
+    "side" | "align" | "sideOffset" | "alignOffset"
   >) {
   return (
     <ComboboxPrimitive.Portal>
@@ -106,13 +105,13 @@ function ComboboxContent({
         sideOffset={sideOffset}
         align={align}
         alignOffset={alignOffset}
-        anchor={anchor}
-        className="isolate z-50"
+        className="z-[9999]"
       >
         <ComboboxPrimitive.Popup
-          data-slot="combobox-content"
-          data-chips={!!anchor}
-          className={cn("group/combobox-content relative max-h-(--available-height) w-(--anchor-width) max-w-(--available-width) min-w-[calc(var(--anchor-width)+--spacing(7))] origin-(--transform-origin) overflow-hidden rounded-lg bg-popover text-popover-foreground shadow-md ring-1 ring-foreground/10 duration-100 data-[chips=true]:min-w-(--anchor-width) data-[side=bottom]:slide-in-from-top-2 data-[side=inline-end]:slide-in-from-left-2 data-[side=inline-start]:slide-in-from-right-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 *:data-[slot=input-group]:m-1 *:data-[slot=input-group]:mb-0 *:data-[slot=input-group]:h-8 *:data-[slot=input-group]:border-input/30 *:data-[slot=input-group]:bg-input/30 *:data-[slot=input-group]:shadow-none data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95", className )}
+          className={cn(
+            "relative max-h-60 w-full min-w-[200px] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md",
+            className
+          )}
           {...props}
         />
       </ComboboxPrimitive.Positioner>
